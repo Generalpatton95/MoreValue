221e2dc327432ec05f9b90e6faf27a58cfd04121
Austin Patton <pattonaustin1995@gmail.com>
2025-12-14 20:36:57 -0600

    Add project files.

---
+++ MoreValue/MoreValuePatch/ValuableObjectPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreValue.MoreValuePatch
+{
+    [HarmonyPatch(typeof(ValuableObject))]
+    internal class ValuableObjectPatch
+    {
+        [HarmonyPatch("DollarValueSetLogic")]
+        [HarmonyPostfix]
+
+        static void IncreaseObjectValuePatch(ref float ___dollarValueCurrent, ref float ___dollarValueOriginal)
+        {
+            ___dollarValueCurrent *= 3f;
+            ___dollarValueOriginal *= 3f;
+        }
+
+    }
+}
---
+++ MoreValue/ObjectValues.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreValue
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ObjectValues : BaseUnityPlugin
+    {
+        private const string modGUID = "Patt.MoreValue";
+        private const string modName = "MoreValue";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ObjectValues Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("More Value is loading...");
+
+            harmony.PatchAll();
+            mls.LogInfo("More Value Loaded successfully!");
+        }
+
+
+
+
+
+
+    }
+}
